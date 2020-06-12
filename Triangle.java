2413a089804b728974b837933a78d7c4c860bfa5
Ysabel Atiencia <isabel.yamileth@hotmail.es>
2020-06-12 15:01:00 -0500

    Write all test cases

--- Triangle.java
+++ Triangle.java
@@ -10,9 +10,55 @@ public class Triangle<E>{
 
 	public static void main(String[] args) {
         Triangle t=new Triangle<>();
-        System.out.println("Test 1: "+ "Equilateral".equals(t.triangleType(3,3,3)));
-		System.out.println("Test 2: "+ "Scalene".equals(t.triangleType(5,4,8)));
-		System.out.println("Test 3: "+ "Isosceles".equals(t.triangleType(9,"n",9)));
+		System.out.println("              Test 1");
+		System.out.println("Equilateral - (3,3,3):   "+ "Equilateral".equals(t.triangleType(3,3,3)));
+		System.out.println("Return: "+ t.triangleType(3,3,3));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 2");
+		System.out.println("Scalene - (5,4,8):   "+ "Scalene".equals(t.triangleType(5,4,8)));
+		System.out.println("Return: "+ t.triangleType(5,4,8));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 3");
+		System.out.println("Isosceles - (9,5,9):   "+ "Isosceles".equals(t.triangleType(9,5,9)));
+		System.out.println("Return: "+ t.triangleType(9,5,9));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 4");
+		System.out.println("Isosceles - (5,'n',3):   "+ "Isosceles".equals(t.triangleType(9,'n',9)));
+		System.out.println("Return: "+ t.triangleType(9,'n',9));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 5");
+		System.out.println("Scalene - (5,4,0):   "+ "Scalene".equals(t.triangleType(5,4,0)));
+		System.out.println("Return: "+ t.triangleType(5,4,0));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 6");
+		System.out.println("Scalene - ('f','v','c'):   "+ "Scalene".equals(t.triangleType('f','v','c')));
+		System.out.println("Return: "+ t.triangleType('f','v','c'));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 7");
+		System.out.println("Scalene - (-,2,4):   "+ "Scalene".equals(t.triangleType('-',2,4)));
+		System.out.println("Return: "+ t.triangleType('-',2,4));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 8");
+		System.out.println("Scalene - (300,300,2):   "+ "Scalene".equals(t.triangleType(300,300,2)));
+		System.out.println("Return: "+ t.triangleType(300,300,2));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 9");
+		System.out.println("Scalene - (1,2,1):   "+ "Scalene".equals(t.triangleType(1,2,1)));
+		System.out.println("Return: "+ t.triangleType(1,2,1));
+		System.out.println("********************************************************");
+		
+		System.out.println("              Test 10");
+		System.out.println("Scalene - (5,5,'?'):   "+ "Scalene".equals(t.triangleType(5,5,'?')));
+		System.out.println("Return: "+ t.triangleType(5,5,'?'));
+		System.out.println("********************************************************");
 		
 	}
 	
